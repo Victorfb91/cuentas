ef6000b4b01af89b8ca73f135bc9c77bf250b840
Victorfb91 <123470027+Victorfb91@users.noreply.github.com>
2023-01-24 15:27:36 +0100

    Add files via upload

---
+++ CCuenta.java
@@ -0,0 +1,134 @@
+package cuentas;
+
+/**
+ *
+ * @author victo
+ */
+public class CCuenta {
+
+    /**
+     *
+     * @return
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     *
+     * @param nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     *
+     * @param cuenta
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     *
+     * @param saldo
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    /**
+     *
+     * @param tipoInterés
+     */
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+    /**
+     *
+     */
+    public CCuenta()
+    {
+    }
+
+    /**
+     *
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public double estado()
+    {
+        return getSaldo();
+    }
+
+    /**
+     *
+     * @param cantidad
+     * @throws Exception
+     */
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(getSaldo() + cantidad);
+    }
+
+    /**
+     *
+     * @param cantidad
+     * @throws Exception
+     */
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+}
